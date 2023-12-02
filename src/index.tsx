b5dface3a8428d7bbb5e9a6073b1f565379be32f
Ícaro <icaropereirasun@gmail.com>
2023-12-02 11:00:07 -0400

    config: import store

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { setupStore } from "./redux/store";
+
+const store = setupStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store = {store}>
       <App />
     </Provider>
   </React.StrictMode>,
